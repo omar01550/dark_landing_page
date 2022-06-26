bf5a78ad2aa54879bacfe5f51f1223b13c6406a3
omar01550 <arom57818@gmail.com>
2022-06-26 19:51:29 +0200

    dark landing page

---
+++ main.js
@@ -0,0 +1,53 @@
+let scrollToTop=document.querySelector(".scroll-to-top");
+
+
+scrollToTop.onclick=function(){
+    scroll();
+}
+
+window.addEventListener("scroll",function(){
+  if(window.scrollY>=600){
+      scrollToTop.style.display="block" ;
+  }else{
+    scrollToTop.style.display="none" ;
+  }
+})
+
+
+
+
+function scroll(){
+   window.scrollTo({
+     top:0,
+     left:0,
+     behavior:"smooth"
+   })
+};
+
+//sections animation
+   //second section
+let sectionSecond=document.querySelector(".second");
+let cards=document.querySelectorAll(".second .card");
+
+
+window.addEventListener("scroll",function(){
+   if(window.scrollY >= sectionSecond.offsetTop-190){
+      cards.forEach((card)=>{
+         card.style.transform="translateY(0)";
+      })
+   }
+})
+
+
+
+//section three
+let sectionThree=document.querySelector(".three");
+let img=document.querySelector(".three img");
+let content=document.querySelector(".three .content");
+
+window.addEventListener("scroll",function(){
+   if(window.scrollY >= sectionSecond.offsetTop-130){
+      content.style.transform="translateX(0)";
+      img.style.transform="translateX(0)";
+   }
+})
